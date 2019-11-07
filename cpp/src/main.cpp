b3f8405212842cd74bbcd4963d1ae9a6cc4ea51f
hanseuljun <hanseuljun@gmail.com>
2019-11-06 16:12:44 -0800

    Update anlysis.

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -66,6 +66,8 @@ std::vector<std::string> get_filenames_from_folder_path(std::string folder_path)
 		std::string filename = entry.path().filename().string();
 		if (filename == ".gitignore")
 			continue;
+		if (entry.is_directory())
+			continue;
 		filenames.push_back(filename);
 	}
 
