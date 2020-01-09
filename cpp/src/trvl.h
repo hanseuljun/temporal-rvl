445a471caf54fe799fe21c001bef59f1116e0b43
hanseuljun <hanseuljun@gmail.com>
2020-01-08 16:23:24 -0800

    Turn trvl into a header library.

--- cpp/src/trvl.h
+++ cpp/src/trvl.h
@@ -19,5 +19,31 @@ private:
     int invalid_count_;
 };
 
-void update_pixel(Pixel& pixel, short raw_value, short change_threshold, int invalidation_threshold);
+void update_pixel(Pixel& pixel, short raw_value, short change_threshold, int invalidation_threshold) {
+    if (pixel.value() == 0) {
+        if (raw_value > 0)
+            pixel.set_value(raw_value);
+
+        return;
+    }
+
+    // Reset the pixel if the depth value indicates the input was invalid two times in a row.
+    if (raw_value == 0) {
+        pixel.set_invalid_count(pixel.invalid_count() + 1);
+        if (pixel.invalid_count() >= invalidation_threshold) {
+            pixel.set_value(0);
+            pixel.set_invalid_count(0);
+        }
+        return;
+    }
+    pixel.set_invalid_count(0);
+
+    short diff = pixel.value() - raw_value;
+    if (diff < 0)
+        diff = -diff;
+
+    // Update pixel value when change is detected.
+    if (diff > change_threshold)
+        pixel.set_value(raw_value);
+}
 }
